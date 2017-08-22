d5083320e8924c2490bdc2abba5c04b48e0d36bb
Veronika Pogrebna <veronika.pogrebna@capitalone.com>
2017-08-22 15:54:59 -0400

    Finished implementing instruct button, it shows and hides instructions to the game. Still need to add the actual instructions

--- js/main.js
+++ js/main.js
@@ -14,21 +14,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // move the pacman depending to the arrow key pressed by user
   document.onkeydown = function(event) {
     event.preventDefault();
+    // right arrow key
     if (event.keyCode == '39') {
-      // right arrow
-      Pacman.moveRight();
+      // only move right if you are within the board
+      if (Board.locations[Pacman.pacmanPosition].column < 19) {
+        Pacman.moveRight();
+      }
     }
+    // left arrow key
     else if (event.keyCode == '37') {
-      // left arrow
-      Pacman.moveLeft();
+      // only move left if you are within the board
+      if (Board.locations[Pacman.pacmanPosition].column > 0) {
+        Pacman.moveLeft();
+      }
     }
+    // down arrow key
     else if (event.keyCode == '40') {
-      // down arrow
-      Pacman.moveDown();
+      // only move down if you are within the board
+      if (Board.locations[Pacman.pacmanPosition].row < 19) {
+        Pacman.moveDown();
+      }
     }
+    // up arrow key
     else if (event.keyCode == '38') {
-      // up arrow
-      Pacman.moveUp();
+      // only move up if you are within the board
+      if (Board.locations[Pacman.pacmanPosition].row > 0) {
+        Pacman.moveUp();
+      }
     }
     else if (event.keyCode == '32') {
       // If spacebar is pressed once, pause the game. If it pressed again unpause. Keep count of the number of presses.
@@ -42,10 +54,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
     }
   }
-
-  // button listeners
+  // BUTTON LISTENERS
+  // New Game button listener (restarts game)
   var $newGameButton = $('#new-game');
   $newGameButton.on('click', function() {
     Game.newGame();
   });
+  // Instructions button listener (shows and hides instructions)
+  var $instructionsButton = $('#instructions-button');
+  var $instructions = $('#instructions');
+  $instructions.hide();
+  $instructionsButton.on('click', function() {
+    if ($instructionsButton.text() === "Show Instructions") {
+      $instructions.show();
+      $instructionsButton.text("Hide Instructions");
+    }
+    else if ($instructionsButton.text() === "Hide Instructions") {
+      $instructions.hide();
+      $instructionsButton.text("Show Instructions");
+    }
+  })
+
+
+
 });
--- prototypes/prototypes.md
+++ prototypes/prototypes.md
@@ -6,6 +6,5 @@ Prototype #2: Pause and unpause game with spacebar. Add gifs for the Pac-man and
 Prototype #3: New Game button lets the user restart the game but keeps the score they have achieved. Highest score determine from game history and how to user. Ghost chases pac-man based on his location.
 
 
-
 IN PROGRESS
-Prototype #4: Don't let pac-man or the ghost to be able to run off the board. Instructions button shows/hides the instructions of the game.
+Prototype #4: Have ghost only skip the divs not add on the dot divs as it goes. Don't let pac-man or the ghost to be able to run off the board. Instructions button shows/hides the instructions of the game. At the end of the game, if the player has at least 150 points, let them know they have won level 0. 
